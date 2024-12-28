538e852f251731207f389df9b8664d555defe786
kiruhi <peterkiruhi4@gmail.com>
2024-12-28 14:54:41 +0300

    Update authStore.js
    Fixed a typo

--- frontend/store/authStore.js
+++ frontend/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 
-const API_URI = "https://authentication-starter-kit.onrender.com/auth/api";
+const API_URI = "https://authentication-starter-kit.onrender.com/api/auth";
 
 axios.defaults.withCredentials = true;
 
