425e1e32d3711cbc56361d04c699d7689350ef40
kiruhi <peterkiruhi4@gmail.com>
2024-12-28 15:03:56 +0300

    Update Server.js
    fixed cross origin issues

--- backend/Server.js
+++ backend/Server.js
@@ -2,26 +2,39 @@ import express from "express";
 import dotenv from "dotenv";
 import logger from "morgan";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-//middleware
-dotenv.config();
-app.use(express.json()); //allow to parse incoming requets: body.req
+// Middleware
+app.use(express.json()); // Parse incoming requests with JSON payloads
 app.use(logger("combined"));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-//Routes
+// CORS configuration
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// Routes
 app.use("/api/auth", authRoutes);
 
+// Start server
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server running at ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
