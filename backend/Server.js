f26f267f6a972faacc75bab79e2b205850393924
kiruhi <peterkiruhi4@gmail.com>
2024-12-28 14:53:30 +0300

    Update Server.js
    Made client uri dynamic

--- backend/Server.js
+++ backend/Server.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(express.json()); //allow to parse incoming requets: body.req
 app.use(logger("combined"));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true}))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 //Routes
 app.use("/api/auth", authRoutes);
